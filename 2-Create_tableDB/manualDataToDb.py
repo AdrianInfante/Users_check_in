7f48609a5061bed906a386598d7d787d6b1db572
Adrian Infante Gonzalez <40429138+AdrianInfante@users.noreply.github.com>
2024-07-03 13:17:02 +0100

    Update manualDataToDb.py

--- 2-Create_tableDB/manualDataToDb.py
+++ 2-Create_tableDB/manualDataToDb.py
@@ -1,14 +1,15 @@
 import sqlite3
+
 result=[
     # (1, '011-0476', 'IQ8712NE-V18', 'A14'),
     # (2, '011-0487', 'IQD62NI-B7', 'A11'),
     # (3, '011-0490', 'IQD61WV-F13', 'D02')
 ]
-# Connect to the database (creates a new file if it doesn't exist)
+
 conn = sqlite3.connect('users.db')
 cursor = conn.cursor()
 
-# Create the 'products' table
+
 create_table_query = '''
 CREATE TABLE `users` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,11 +20,6 @@ CREATE TABLE `users` (
 '''
 cursor.execute(create_table_query)
 
-# Insert values into the 'products' table
-# Values to be inserted
-
-
-# Insert values into the 'products' table
 insert_query = '''
     INSERT INTO users (`id`, `name`, `status`, `date`)
     VALUES (?, ?, ?, ?)
@@ -31,10 +27,6 @@ insert_query = '''
 
 cursor.executemany(insert_query, result)
 
-
-
-
-# Commit changes and close the connection
 conn.commit()
 conn.close()
 
